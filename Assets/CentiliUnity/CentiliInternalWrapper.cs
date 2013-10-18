65b6d173a4ce53e2446fdb4be8c70b0221361a95
Nikola Maric <Nikola.Maric@infobip.com>
2013-10-18 11:55:02 +0200

    Initial commit

---
+++ Assets/CentiliUnity/CentiliInternalWrapper.cs
@@ -0,0 +1,146 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using CentiliUnity;
+
+public class CentiliInternalWrapper	: MonoBehaviour {
+	
+	private CentiliPaymentResponse lastCentiliPaymentResponse;
+	private CentiliPaymentStatus lastCentiliPaymentStatus;
+	
+	public bool Logging { get; set; }
+	public void SetLoggingEnabled(bool logging) 
+	{
+		Logging = logging;
+	}
+	
+	public void MakePayment(CentiliPaymentRequest request, Action<CentiliPaymentStatus, CentiliPaymentResponse> callback)
+	{
+		StartCoroutine(MakePaymentCoroutine(request, callback));
+	}
+	
+	/// <summary>
+	/// MakePayment coroutine that can wait until the CentiliPaymentResponse is ready
+	/// </summary>
+	private IEnumerator MakePaymentCoroutine(CentiliPaymentRequest request, Action<CentiliPaymentStatus, CentiliPaymentResponse> callback)
+	{
+		// invoke singleton instance creation of this object, because we need it
+		GetInstance();
+		
+		// Passing data over the bridge between C# here and Java running on Android has
+		// a quirk, that it can't handle NULL values and throws exceptions. Here is a
+		// workaround that uses an extra string to pass which variables are present and which
+		// are absent.
+		string presenceMask = new string(new char[]
+		{
+			request.ApiKey == null					? 'a' : 'p',
+			request.PackageIndex == null		? 'a' : 'p',
+			request.LanguageCode == null		? 'a' : 'p',
+			request.Info == null						? 'a' : 'p',
+			request.ClientId == null 				? 'a' : 'p',
+			request.TestModeEnabled == null	? 'a' : 'p',
+			request.OfflineModeEnabled == null	? 'a' : 'p',
+			request.PendingTransactionHandlingEnabled == null ? 'a' : 'p'
+		});
+
+		// Java side will use this parameter to return data
+		lastCentiliPaymentResponse = null;
+		
+		// A method with a lot of parameters is acceptable here, because other options add even
+		// more bloat and won't help with readability (serializations)
+		GetCurrentActivity().Call("startPayment", new object[] 
+		{
+			presenceMask,
+			request.ApiKey == null					? "" : request.ApiKey,
+			request.PackageIndex == null		? (int)-1 : (int) request.PackageIndex,
+			request.LanguageCode == null		? "EN" : request.LanguageCode,
+			request.Info == null						? "" : request.Info,
+			request.ClientId == null 				? "" : request.ClientId,
+			request.TestModeEnabled == null	? true : request.TestModeEnabled,
+			request.OfflineModeEnabled == null	? true : request.OfflineModeEnabled,
+			request.PendingTransactionHandlingEnabled == null ? false : request.PendingTransactionHandlingEnabled
+		});
+		
+		// if there is no callback, don't wait for response and return immediately
+		if (callback == null)
+		{
+			yield break;
+		}
+		
+		// wait until we get a result. This variable is set in NotifyResponseReady()
+		// that is called from Android Java runtime
+		while (lastCentiliPaymentResponse == null) 
+		{
+			yield return 1; // wait 1 frame
+		}
+		
+		// call the callback and clear the buffer automatically
+		callback(lastCentiliPaymentStatus, lastCentiliPaymentResponse);
+		lastCentiliPaymentResponse = null;
+	}
+		
+	// Just ignore PENDING notification
+	public void SignalPaymentPending(string voidArg0)
+	{
+		// CentiliPaymentResponse response = new CentiliPaymentResponse(GetCurrentActivity());
+		
+		lastCentiliPaymentStatus = CentiliPaymentStatus.PAYMENT_PENDING;
+		// lastCentiliPaymentResponse = response;
+	}
+	
+	public void SignalPaymentFailed(string voidArg0)
+	{
+		CentiliPaymentResponse response = new CentiliPaymentResponse(GetCurrentActivity());
+		
+		lastCentiliPaymentStatus = CentiliPaymentStatus.PAYMENT_FAILED;
+		lastCentiliPaymentResponse = response;
+	}
+	
+	public void SignalPaymentCanceled(string voidArg0)
+	{
+		CentiliPaymentResponse response = new CentiliPaymentResponse(GetCurrentActivity());
+		
+		lastCentiliPaymentStatus = CentiliPaymentStatus.PAYMENT_CANCELED;
+		lastCentiliPaymentResponse = response;
+	}
+	
+	public void SignalPaymentSuccessful(string voidArg0)
+	{
+		CentiliPaymentResponse response = new CentiliPaymentResponse(GetCurrentActivity());
+		
+		lastCentiliPaymentStatus = CentiliPaymentStatus.PAYMENT_SUCCESSFUL;
+		lastCentiliPaymentResponse = response;
+	}
+
+	private const string SINGLETON_GAME_OBJECT_NAME = "Centili Instance";
+	
+	public static CentiliInternalWrapper Instance 
+	{
+		get 
+		{
+			return GetInstance();
+		}
+	}
+	
+	private static CentiliInternalWrapper _instance = null;
+	
+	private static CentiliInternalWrapper GetInstance() 
+	{
+		if (_instance == null) 
+		{
+			_instance = FindObjectOfType(typeof(CentiliInternalWrapper)) as CentiliInternalWrapper;
+			if (_instance == null)
+			{
+				var gameObject = new GameObject(SINGLETON_GAME_OBJECT_NAME);
+				_instance = gameObject.AddComponent<CentiliInternalWrapper>();
+			}
+		}
+		return _instance;
+	}	
+
+	private static AndroidJavaObject GetCurrentActivity()
+	{
+		AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
+		return unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+	}
+}
---
+++ Assets/CentiliUnity/CentiliUnity.cs
@@ -0,0 +1,151 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+///
+/// </summary>
+namespace CentiliUnity 
+{
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	public class CentiliPaymentRequest 
+	{
+		public string ApiKey;
+		public int PackageIndex { get; set; }
+		public string LanguageCode { get; set; }
+		public string Info { get; set; }
+		public string ClientId { get; set; }
+		public bool TestModeEnabled { get; set; }
+		public bool OfflineModeEnabled { get; set; }
+		public bool PendingTransactionHandlingEnabled { get; set; }
+		
+		public CentiliPaymentRequest(string ApiKey) 
+		{
+			this.ApiKey = ApiKey;
+			this.PackageIndex = -1;
+		}
+
+		public override string ToString() 
+		{
+			var request = this;
+			return string.Format("CentiliPaymentRequest: {\n\t{0},\n\t{1},\n\t{2},\n\t{3},\n\t{4},\n\t{5},\n\t{6}\n\t{7}}",
+				"ApiKey: " + request.ApiKey == null										? "" : request.ApiKey,
+				"PackageIndex: " + request.PackageIndex == null					? (int)(-1) : (int) request.PackageIndex,
+				"LanguageCode: " + request.LanguageCode == null					? "EN" : request.LanguageCode,
+				"Info: " + request.Info == null												? "" : request.Info,
+				"ClientId: " + request.ClientId == null 									? "" : request.ClientId,
+				"TestModeEnabled: " + request.TestModeEnabled == null			? true : request.TestModeEnabled,
+				"OfflineModeEnabled: " + request.OfflineModeEnabled == null	? true : request.OfflineModeEnabled,
+				"PendingTransactionHandlingEnabled: " + request.PendingTransactionHandlingEnabled == null ? false : request.PendingTransactionHandlingEnabled
+			);
+		}
+	}
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	public class CentiliPaymentResponse 
+	{
+		
+		public CentiliPaymentResponse(AndroidJavaObject activity)
+		{
+			ApiKey = activity.Get<string>("responseApiKey");
+			ClientId = activity.Get<string>("responseClientId");
+			Price = activity.Get<double>("responsePrice");
+			ItemAmount = activity.Get<int>("responseItemAmount");
+			ItemName = activity.Get<string>("responseItemName");
+			Currency = activity.Get<string>("responseCurrency");
+			TransactionId = activity.Get<string>("responseTransactionId");
+			ErrorMessage = activity.Get<string>("responseErrorMessage");
+		}
+		
+		// Amount of credits paid for by the user (if the user buys "3 swords" then this value is "3").
+		public int ItemAmount;
+		
+		// Name of the credits paid for by the user (in case of "3 swords" this would be "swords").
+		public string ItemName;
+
+		// Currency in which transaction was processed
+		public string Currency;
+		
+		// Id of the client
+		public string ClientId;
+		
+		// Amount paid by the user in a fractional format (X.YZ)
+		public double Price;
+		
+		// API KEY
+		public string ApiKey;
+		
+		// Transaction id
+		public string TransactionId;
+		
+		// Error message (null if no error)
+		public string ErrorMessage;
+		
+		// Interval is omitted because subscription payment method is not used
+		// public int Interval;
+		
+		public override string ToString() {
+			return string.Format("CentiliPaymentResponse: {\n\t" +
+				"ApiKey:{0},\n\tTransactionId:{1},\n\tItemAmount:{2},\n\t" +
+				"ItemName:{3},\n\tPrice:{4},\n\tCurrency:{5},\n\t" +
+				"ClientId:{6},\n\tErrorMessage:{7}\n}",
+				ApiKey, TransactionId, ItemAmount, 
+				ItemName, Price, Currency,
+				ClientId, ErrorMessage);
+		}
+	}
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	public enum CentiliPaymentStatus 
+	{
+		// Shouldn't ever get PENDING status (in end-user code)
+		PAYMENT_PENDING = 0,
+		PAYMENT_FAILED = 1,
+		PAYMENT_CANCELED = 2,
+		PAYMENT_SUCCESSFUL = 3
+	}
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	public class CentiliPaymentManager {
+				
+		/// <summary>
+		/// 
+		/// </summary>
+		/*public static void SetLoggingEnabled(bool loggingEnabled)
+		{
+			if(Application.platform == RuntimePlatform.Android) {
+				CentiliInternalWrapper.SetLoggingEnabled(loggingEnabled);
+			}
+		}
+		*/
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name='request'>
+		/// the CentiliPaymentRequest object for the payment
+		/// </param>
+		/// <param name='callback'>
+		/// the callback method on submitting request for payment
+		/// </param>
+		public static void MakePayment(CentiliPaymentRequest request, Action<CentiliPaymentStatus, CentiliPaymentResponse> callback)
+		{
+			if(Application.platform == RuntimePlatform.Android) {
+				CentiliInternalWrapper.Instance.MakePayment(request, callback);
+			} else {
+				// your mockup code here
+			}
+		}
+
+	}
+}
