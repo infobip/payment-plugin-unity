7b6b73d2bf5fda011da3b8ce705aae118644e27e
Nikola Maric <Nikola.Maric@infobip.com>
2014-01-24 13:08:41 +0100

    Updated with new lib. Added demo scene.

---
+++ Assets/CentiliUnityDemo/CentiliUnityDemo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using CentiliUnity;
+
+public class CentiliUnityDemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnGUI()
+	{
+	    if (GUILayout.Button("Test Centili Unity Payment"))
+	    {
+            print("Request for payment sent.");
+			var paymentRequest = new CentiliPaymentRequest("28550ec26491d4ed1b1de6fd3fe2b92a");
+			paymentRequest.ClientId = "test";
+			CentiliPaymentManager.DebugMode = true;
+			CentiliPaymentManager.MakePayment(paymentRequest, CentiliCallback);
+	    }
+	}
+
+	private void CentiliCallback(CentiliPaymentStatus status, CentiliPaymentResponse response)
+    {
+        print(status);
+        print(response.ItemAmount);
+    }
+}
