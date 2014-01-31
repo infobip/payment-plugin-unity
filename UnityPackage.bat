9216f5dd2826f0fdadbc35f762717b67c836382e
Nikola Maric <Nikola.Maric@infobip.com>
2014-01-31 16:54:42 +0100

    removed unused file

--- UnityPackage.bat
+++
@@ -1,5 +0,0 @@
-mkdir %1
-echo %1
-"C:\Program Files (x86)\Unity\Editor\Data\Tools\7z.exe" "a" "-r" "-ttar" "-y" "-bd" %1\archtemp.tar "Assets"
-"C:\Program Files (x86)\Unity\Editor\Data\Tools\7z.exe" "a" "-tgzip" "-bd" "-y" %1\CentiliUnity.unitypackage %1\archtemp.tar
-del %1\archtemp.tar
